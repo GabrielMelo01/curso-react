ad9e05bb7d6e7bc832bc9251a1891e74990bc527
GabrielMelo01 <borges.gm22@gmail.com>
2024-05-23 14:57:44 -0300

    Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import './home.css'
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
@@ -10,20 +12,31 @@ function Home() {
         params: {
           language: "pt-BR",
           api_key: "d2781c36534e85516b4f4f7ce9d04fdc",
-          page: 1
+          page: 1,
+          size: 10
         }
       })
-      console.log(response.data.results);
-      // const data = await response.json();
-      // setFilmes(data);
+      // console.log(response.data.results.slice(0,10));
+      setFilmes(response.data.results.slice(0,10));
     }
     loadFilmes();
   }, []);
 
     return (
-      <div>
-        <h1>My React Home</h1>
-        <br/>
+      <div className="container">
+        <div className="lista-filmes">
+          {filmes.map((filme) => {
+            return (
+              <article key={filme.id}>
+                <strong>{filme.title}</strong>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${filme.poster_path}`}
+                  alt={filme.title}/>
+                  <Link to={`/filme/${filme.id}`}>Acessar</Link>
+              </article>
+            );
+          })}
+        </div>
       </div>
     );
 }
