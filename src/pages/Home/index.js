c64c04e407783547d9bd2b5b432712ae6aead261
GabrielMelo01 <borges.gm22@gmail.com>
2024-05-23 14:37:22 -0300

    Criando nossa home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("/movie/now_playing", {
+        params: {
+          language: "pt-BR",
+          api_key: "d2781c36534e85516b4f4f7ce9d04fdc",
+          page: 1
+        }
+      })
+      console.log(response.data.results);
+      // const data = await response.json();
+      // setFilmes(data);
+    }
+    loadFilmes();
+  }, []);
+
     return (
       <div>
         <h1>My React Home</h1>
