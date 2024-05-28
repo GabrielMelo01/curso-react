9e5a68df0a3410c80a007bedaee7c7678f0746ee
GabrielMelo01 <borges.gm22@gmail.com>
2024-05-28 11:07:11 -0300

    efeito transparencia header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './header.css'
+import './header.css';
+
 function Header() {
+    const [headerOpacity, setHeaderOpacity] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const maxScroll = 200; // Defina o valor máximo de scroll para opacidade máxima
+            const opacity = Math.min(scrollY / maxScroll, 1);
+            setHeaderOpacity(opacity);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header>
-        <Link className="logo" to='/'>Prime Flix</Link>
-        <Link className="favoritos" to='/favoritos'>Minha Conta</Link>
-      </header>
+        <header style={{ backgroundColor: `rgba(0, 0, 0, ${headerOpacity > 0.75 ? 0.75 : headerOpacity})`, backdropFilter: 'blur(10px)' }}>
+            <Link className="logo" to='/'>Prime Flix</Link>
+            <Link className="favoritos" to='/favoritos'>Minha Conta</Link>
+        </header>
     );
 }
+
 export default Header;
